0c33198c1f3452ed44a157416fdebecffa2f9585
DhruvMeduri <mdhruv@iisc.ac.in>
2023-11-27 13:19:48 -0700

    changed the tcav computation

--- our_method.py
+++ our_method.py
@@ -75,14 +75,15 @@ def assemble_concept(name, id, concepts_path="./data/tcav/image/concepts/"):
 
 model = torchvision.models.googlenet(pretrained=True)
 model = model.eval()
+#print(model.categories)
 '''
-#print(model.inception4c)
+#print(model.inception4d)
 #for i in model.modules():
 #    print(i)
 
 #This is one way to do it 
 
-layer_act = LayerActivation(model, model.inception4c)
+layer_act = LayerActivation(model, model.inception4d)
 #model = model.eval()
 
 zebra_imgs = load_image_tensors('zebra', transform=False)#
@@ -99,15 +100,15 @@ print(attribution.shape)
 
 concepts_path = "./data/tcav/image/concepts/"
 
-stripes_concept = assemble_concept("striped", 0, concepts_path=concepts_path)
-zigzagged_concept = assemble_concept("zigzagged", 1, concepts_path=concepts_path)
+stripes_concept = assemble_concept("caucasian_small", 0, concepts_path=concepts_path)
+zigzagged_concept = assemble_concept("african_small", 1, concepts_path=concepts_path)
 dotted_concept = assemble_concept("dotted", 2, concepts_path=concepts_path)
 
 
 random_0_concept = assemble_concept("random_0", 3, concepts_path=concepts_path)
 random_1_concept = assemble_concept("random_1", 4, concepts_path=concepts_path)
 
-layers=['inception4c', 'inception4d', 'inception4e']
+layers=['inception4c', 'inception4d', 'inception4e','inception5a','inception5b']
 
 mytcav = TCAV(model=model,
               layers=layers,
@@ -118,8 +119,8 @@ mytcav = TCAV(model=model,
 
 dic = mytcav.generate_activation(layers,stripes_concept)
 
-temp_1 = torch.load('./cav/av/default_model_id/striped-0/inception4d/0.pt')# For striped
-temp_2 = torch.load('./cav/av/default_model_id/striped-0/inception4d/1.pt')# For striped
+temp_1 = torch.load('./cav/av/default_model_id/caucasian_small-0/inception5a/0.pt')# For striped
+temp_2 = torch.load('./cav/av/default_model_id/caucasian_small-0/inception5a/1.pt')# For striped
 concept_activations = torch.cat((temp_1,temp_2))
 concept_activations = np.array(concept_activations)
 #print(len(concept_activations))
@@ -132,10 +133,10 @@ concept_labels = np.array(concept_labels)
 
 #For getting the activations of the random set
 
-dic = mytcav.generate_activation(layers,dotted_concept)
+dic = mytcav.generate_activation(layers,zigzagged_concept)
 
-temp_1 = torch.load('./cav/av/default_model_id/dotted-2/inception4d/0.pt')# For random concept
-temp_2 = torch.load('./cav/av/default_model_id/dotted-2/inception4d/1.pt')# For random concept
+temp_1 = torch.load('./cav/av/default_model_id/african_small-1/inception5a/0.pt')# For random concept
+temp_2 = torch.load('./cav/av/default_model_id/african_small-1/inception5a/1.pt')# For random concept
 random_activations = torch.cat((temp_1,temp_2))
 random_activations = np.array(random_activations)
 random_labels = []
@@ -149,27 +150,50 @@ labels = np.concatenate((concept_labels,random_labels))
 
 # Now to the SVM
 print("SVM Running")
-clf = svm.SVC(kernel='linear',probability=True)
+clf = svm.SVC(kernel='rbf',probability=True)
 clf.fit(activations, labels)
 print(clf.intercept_)
 
 # Now to get the gradients of the zebra class inputs
-
-zebra_imgs = load_image_tensors('zebra', transform=False)#
+layer_act = LayerActivation(model, model.inception5a)
+zebra_imgs = load_image_tensors('rugby_small', transform=False)#
 zebra_tensors = torch.stack([transform(img) for img in zebra_imgs])
-#print(zebra_tensors.shape)
-#input = torch.randn(2, 3, 32, 32, requires_grad=True)
-print("Gradient")
-grad= LayerIntegratedGradients(model,model.inception4d)
-check = grad.attribute(zebra_tensors,target=340,n_steps=1)
-check = torch.reshape(check,(check.shape[0],activations.shape[1]))
-check = check.detach().numpy()
-print(check.shape)
+
+
+# Predicted class value using argmax
+prediction = model(zebra_tensors)
+prediction = prediction.detach().numpy()
+predicted_class = np.argmax(prediction,axis=1)
+print(predicted_class)
+#print("Gradient")
+#grad= LayerIntegratedGradients(model,model.inception5a)
+#check = grad.attribute(zebra_tensors,target=340,n_steps=1)
+#check = torch.reshape(check,(check.shape[0],activations.shape[1]))
+print("Activations")
+attribution = layer_act.attribute(zebra_tensors)
+attribution = torch.reshape(attribution,(attribution.shape[0],attribution.shape[1]*attribution.shape[2]*attribution.shape[3]))
+
+attribution = attribution.detach().numpy()
+print(attribution.shape)
 
 # This is the notation of the paper
-S = clf.decision_function(check)-clf.intercept_
+#S = clf.decision_function(check)-clf.intercept_
+#count = 0 
+#for i in S:
+#    if i>=0:
+#        count = count + 1
+#print("Relative TCAV: ",count/len(S))
+
+#print(clf.predict(attribution))
+pred = clf.predict(attribution)
 count = 0 
-for i in S:
-    if i>=0:
+for i in range(len(pred)):
+    if pred[i] == 1:
+        #if predicted_class[i] == 768:
         count = count + 1
-print("Relative TCAV: ",count/len(S))
+
+print("Score: ", count/len(pred))
+
+
+
+
